54f31fca157dffa46703fb19823fc30003cece1a
errorcore101 <vakho713@icloud.com>
2025-07-11 18:37:54 +0400

    commit

---
+++ day01/hm/lesson01.py
@@ -0,0 +1,16 @@
+print("taco drawing purple")
+
+movie1 = "a minecraft movie"
+movie2 = "sonic 2"
+movie3 = "Ratatouille"
+movie4 = "dispicable me 4"
+movie5 = "Lorax"
+
+#ურთიერთდაკავშირებული ნივთების სერია
+
+#ბრჭყალები, ცვლადები, ფრჩხილები, int, str.
+
+name = "vakho"
+age = "12"
+
+print("my name is" + " " + name + " " + "and i'm" + age + " " + "years old.")
---
+++ day02/hm/lesson02.py
@@ -0,0 +1,15 @@
+#ეს პირველი არის შესწორება
+
+name = "luka"
+birthday = "2015"
+print("მოგესალმებით," + " " + name + "," + " " + "დაბადების დღე გაქქვს" + birthday)
+
+
+score = "999"
+print(score)
+health = "100"
+print(health)
+dad = "zura vardosanidze"
+mom = "eka pxakadze"
+grandma = "aniko eristavi"
+print(dad + " " + mom + " " + grandma)
