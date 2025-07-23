1a3c84c1b67115a1fea147b21b0069e681bda7ae
errorcore101 <vakho713@icloud.com>
2025-07-23 13:55:28 +0400

    commit

---
+++ day06/cw.py
@@ -0,0 +1,11 @@
+print("hello" + " " + "there")
+
+print("ELLO GOVNA")
+print(34 + 34)
+print(46 != 67)
+
+integertostring = str(56)
+print(integertostring)
+
+stringtoineger = int("90")
+print(stringtoineger)
---
+++ day06/hw.py
@@ -0,0 +1,34 @@
+#str არის ტექსტი რომელიც მარტო მუშაობს როცა ბრჭყალებში ცასვამ
+#int არის მთელი ციფრები
+#float არის ციფრები მაგრემ ამ ჯერას მეატეები ემატება
+#boolean არის შედარება რომელიც იყენებს True, False. True როცა მართალია, False როცა ტყვილია.
+
+print("hello!")
+print("HA-HAHA!! ARE YOU KIDING? I HATE THIS STUPID JOB")
+print("might as well give up! i'm [TITLE CARD]")
+print("i'm going to drink water! its been a while since i've done")
+print("99 percent of gamblers quit right before they DIE")
+print(9 + 6)
+print(345 + 5894)
+print(2365 - 356)
+print(1000000 / 1000)
+print(894375 * 100)
+print(8.2 + 324.8)
+print(732.5 - 94.7)
+print(450.5 * 120.7)
+print(180.5 / 9.5)
+print(78.2 % 29.9)
+print(70 > 40)
+print(90 < 140)
+print(347 == 347)
+print(293 != 323)
+print(2938 >= 32423)
+
+input(345 + 5894)
+input(9 + 6)
+input(6239 > 15)
+
+#ეს იქნება true
+print(902 < 10000)
+#და ეს იქნება false
+print(8932 > 21903)
