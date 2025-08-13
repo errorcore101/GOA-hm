d7ba8afbf3e9f1f781089f73dabee35a68d42d8a
errorcore101 <vakho713@icloud.com>
2025-08-13 11:53:33 +0400

    commit

---
+++ day11/hw.py
@@ -0,0 +1,28 @@
+#კი, ინდენტაცია არის საჭირო
+for i in range(3):
+    print("ეს არის for loop-ში ანუ ინდენტაცია აქვს")
+    print("ესეც არის for loop-ში")
+print("ეს არ არის for loop-ში ანუ ინდენტაცია არ აქვს")
+
+
+
+for i in range(7):
+    print("4")
+
+
+
+for i in range(8):
+    print("i love cats")
+
+
+
+input("vakho")
+name = "vakho"
+for i in range(5):
+    print("hello" + " " + name)
+
+
+
+#ეს არის შეცდომის გასწორება: for i in range5: print("i")
+for i in range(5):
+    print("i")
