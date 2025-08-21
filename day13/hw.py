229edffef151bdd9f0e1d3e5ceebc67bccdfec95
errorcore101 <vakho713@icloud.com>
2025-08-21 15:47:17 +0400

    commit

---
+++ day13/hw.py
@@ -0,0 +1,55 @@
+#for loop-ი იმეორებს იმ წინანდადებას რასაც ჩაწერ იმ რიცხვამდე რაც შენ აირჩიე
+
+
+
+
+i = int(input("input num:"))
+if i > 0:
+    input("number is positive")
+elif i == 0:
+    input("number is 0")
+elif i < 0:
+    input("number is negative")
+
+
+b = int(input("input number:"))
+if b % 2:
+    input("number is even")
+else:
+    input("number is odd")
+
+
+
+a = int(input("input score:"))
+if a == 100:
+    input("well done!")
+elif a < 50:
+    input("you failed")
+
+
+
+u = int(input("input number:"))
+if u % 2:
+    input("number is even")
+else:
+    input("number is odd")
+
+if u % 10:
+    input("number is even")
+else:
+    input("number is odd")
+
+if u % 20:
+    input("number is even")
+else:
+    input("number is odd")
+
+if u % 30:
+    input("number is even.")
+else:
+    input("number is odd")
+
+if u % 40:
+    input("number is even.")
+else:
+    input("number is odd")
---
+++ day15/cw.py
@@ -0,0 +1,18 @@
+for i in range(10, 50, 5):
+    print(i)
+
+
+
+i = 0
+while i < 50:
+    print("vakho")
+    i = i + 1
+
+
+
+
+
+NAME_LIST = "david", "nick", "jack"
+print(NAME_LIST[0])
+print(NAME_LIST[1])
+print(NAME_LIST[2])
