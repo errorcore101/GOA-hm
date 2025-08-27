3d18aa65522e19bc134df6811013cbfc7e6f5c15
errorcore101 <vakho713@icloud.com>
2025-08-27 11:01:56 +0400

    commit

---
+++ day15/hw.py
@@ -0,0 +1,15 @@
+#while loop-ი იმეორებს მითითებულ მოქმედებას, სანამ  მითითებულ მდგომარეობა არ იქნება.
+#list-ი არის ცხრილი.
+
+i = 4
+while i < 84:
+    i = i + 6
+    print(i)
+
+
+anime_list = ["blue_lock", "dragon_ball_Z", "naruto", "invincible", "one_piece", "bleach"]
+print(anime_list [1])
+print(anime_list[3])
+print(anime_list[5])
+
+#ცვლადს აქვს მარტო ერთი მნიშვნელობა აქვს, ხოლო ლისტს აქვცს 1-ზე მეტი მნიშვენლობები, რომლებიც შეგიძლია აირჩიო რომელი დაბეჭდო
