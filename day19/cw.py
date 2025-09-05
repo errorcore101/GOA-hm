e445bb6bd7b710f6c1ee0aff420ca851f8746da5
errorcore101 <vakho713@icloud.com>
2025-09-05 18:09:06 +0400

    commit

---
+++ day19/cw.py
@@ -0,0 +1,15 @@
+for i in range(50):
+    print(i)
+    if i % 2 == 0:
+        print(str(i) + " luwi")
+    else:
+        print(str(i) + " kenti")
+
+
+
+
+
+randomlist = "1: peaches", "2: the mario movie", "3: heat death of the universe", "4: diabities", "5: terrifier"
+
+for i in range(5):
+    print(randomlist[i])
